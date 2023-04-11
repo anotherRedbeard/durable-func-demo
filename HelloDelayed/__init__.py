9665e99bfde47608299d84997842c2d93ce24e23
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2023-04-11 10:45:38 -0500

    add durable function

---
+++ HelloDelayed/__init__.py
@@ -0,0 +1,23 @@
+# This function is not intended to be invoked directly. Instead it will be
+# triggered by an orchestrator function.
+# Before running this sample, please:
+# - create a Durable orchestration function
+# - create a Durable HTTP starter function
+# - add azure-functions-durable to requirements.txt
+# - run pip install -r requirements.txt
+
+import datetime
+import time
+import azure.durable_functions as df
+import azure.functions as func
+
+
+def main(context: func.Context, name: str) -> str:
+    timeout = datetime.timedelta(seconds=10)
+    deadline = datetime.datetime.utcnow() + timeout
+
+    while datetime.datetime.utcnow() < deadline:
+        time.sleep(1)
+
+    #raise Exception("ActivityFunction timed out")
+    return f"Hello delayed {name}!"
---
+++ HelloOrchestrator/__init__.py
@@ -0,0 +1,26 @@
+# This function is not intended to be invoked directly. Instead it will be
+# triggered by an HTTP starter function.
+# Before running this sample, please:
+# - create a Durable activity function (default name is "Hello")
+# - create a Durable HTTP starter function
+# - add azure-functions-durable to requirements.txt
+# - run pip install -r requirements.txt
+
+from datetime import timedelta
+import datetime
+import logging
+import json
+import time
+
+import azure.functions as func
+import azure.durable_functions as df
+
+
+def orchestrator_function(context: df.DurableOrchestrationContext):
+    logging.info("Orchestration function started")
+    result1 = yield context.call_activity('Hello', "Tokyo")
+    result2 = yield context.call_activity('HelloDelayed', "Seattle")
+    result3 = yield context.call_activity('Hello', "London")
+    return [result1, result2, result3]
+
+main = df.Orchestrator.create(orchestrator_function)
